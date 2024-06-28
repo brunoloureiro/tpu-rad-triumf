3c7bb2e95d4ba19a0910e430e4cb41d38a6161f9
Bruno Loureiro <brunoloureirocoelho@gmail.com>
2024-06-28 15:10:47 +0200

    Models, inputs, and golden

---
+++ scp_24.sh
@@ -0,0 +1,3 @@
+scp ./data/models/* carol@192.168.1.24:~/tpu-rad/models/.
+scp ./data/inputs/* carol@192.168.1.24:~/tpu-rad/inputs/.
+scp ./data/golden/* carol@192.168.1.24:~/tpu-rad/golden/.
---
+++ scp_25.sh
@@ -0,0 +1,3 @@
+scp ./data/models/* carol@192.168.1.25:~/tpu-rad/models/.
+scp ./data/inputs/* carol@192.168.1.25:~/tpu-rad/inputs/.
+scp ./data/golden/* carol@192.168.1.25:~/tpu-rad/golden/.
